bf8461825a5dc400510f3004bdc871a02ad7517e
Shubham Badekar <shubhambadekar7@gmail.com>
2026-07-06 16:20:27 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -87,4 +87,7 @@ app.post('/api/requests', async (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Express App active and monitoring Port: ${PORT}`);
+
+  module.exports = app;
 });
+
